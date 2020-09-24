530e80e2da08777f4bc68290dda16ba787b5f87d
kyvangka1610 <kyvangka2002@gmail.com>
2020-09-24 07:44:23 +0700

    Bring Up Pixys Support
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- pixys_ginkgo.mk
+++ pixys_ginkgo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2020 The LineageOS Project
+# Copyright (C) 2019-2020 The Pixys Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,12 +13,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixys stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 $(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := pixys_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
