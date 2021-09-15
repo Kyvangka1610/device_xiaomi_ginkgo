217da057dd89aeff29e754980a72c4a92b422a56
kyvangka1610 <kyvangka2002@gmail.com>
2021-09-15 10:59:26 +0700

    ginkgo: bringup WaveOS
    Signed-off-by: kyvangka1610 <kyvangka2002@gmail.com>

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -52,6 +52,9 @@ void vendor_load_properties()
         model = "Redmi Note 8";
     }
 
+    // Set Maintainer Name for Wave-OS
+    property_override("ro.wave.maintainer", "Thago(Lohit)");
+
     // Override all partitions' props
     string prop_partitions[] = { "", "odm.", "product.", "system.",
 					"system_ext.", "bootimage.", "vendor." };
